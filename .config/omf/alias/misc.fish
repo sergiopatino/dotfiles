376b39fddbf11411d9cbcfefa4f15c57ae647e1b
Sergio Patino <sergio@vertigobox.org>
2018-11-16 22:13:23 -0800

    Pip alias rm

--- .config/omf/alias/misc.fish
+++ .config/omf/alias/misc.fish
@@ -22,16 +22,6 @@ alias le="/usr/local/share/nvim/runtime/macros/less.sh"
 # SSH
 alias ss="/usr/local/bin/ssh"
 
-# Fasd
-#alias a='fasd -a'        # any
-#alias s='fasd -si'       # show / search / select
-#alias d='fasd -d'        # directory
-#alias f='fasd -f'        # file
-#alias sd='fasd -sid'     # interactive directory selection
-#alias sf='fasd -sif'     # interactive file selection
-#alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-#alias zz='fasd_cd -d -i' # cd with interactive selection
-
 # Test Kitchen
 alias k="kitchen"
 alias ks="kitchen status"
@@ -41,9 +31,6 @@ alias tf="terraform"
 alias tfp="terraform plan"
 alias tfa="terraform apply"
 
-# Pip Upgrade all packages
-alias pipup='pip freeze --local | grep -v "^\-e" | cut -d = -f 1  | xargs -n1 pip install -U'
-
 # History
 alias h="history"
 alias hm="history --merge"
@@ -52,8 +39,3 @@ alias hs="history search"
 
 # Clear
 alias c="clear"
-
-# SSH tunnels
-#alias tunnelqdcjb="ssh -L 7999:qymprdctgts01.corp.intuit.net:799 opdssdtis301.corp.intuit.net -N"
-#alias mediaout="ssh -p 8080 tomato.vertigobox.org"
-#alias mediatunnel="ssh -o ServerAliveInterval=15 -o ServerAliveCountMax=2 -p 8080 tomato.vertigobox.org -L 58846:localhost:58846 -L 5900:localhost:5900 -L 6600:media:6600"
