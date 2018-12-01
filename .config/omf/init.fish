8724579c27a62dbe51150838ca4a2443b900588e
Sergio Patino <sergio@vertigobox.org>
2018-12-01 14:13:38 -0800

    Updaing envvars and moving to omf dev channel

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -13,18 +13,8 @@ for file in $OMF_CONFIG/completions/*.fish
     source $file
 end
 
-# GOPATH
-set -gx GOPATH $HOME/code/go
-set PATH $GOPATH/bin $PATH
-
 # Add local bin path
 set PATH ~/.local/bin/ $PATH
-
-# Pyenv
-status --is-interactive; and source (pyenv init -|psub)
-
-# Rbenv
-status --is-interactive; and source (rbenv init - --no-rehash|psub)
 
 # Clear duplicate entries in PATH
 varclear PATH
