39bea98ec543376de5ccda5542164c5b62a1328d
Sergio Patino <sergio@vertigobox.org>
2019-03-03 02:36:27 -0800

    fix fish files

--- .config/omf/envvars/pyenv.fish
+++ .config/omf/envvars/pyenv.fish
@@ -1,5 +1,6 @@
 # Pyenv
 if type -q pyenv
     status --is-interactive; and source (pyenv init -|psub)
-    [ -f /usr/local/share/fish/vendor_completions.d/pyenv.fish ]; and source /usr/local/share/fish/vendor_completions.d/pyenv.fish
 end
+
+[ -f /usr/local/share/fish/vendor_completions.d/pyenv.fish ]; and source /usr/local/share/fish/vendor_completions.d/pyenv.fish
