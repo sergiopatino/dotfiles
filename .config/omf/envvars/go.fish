6ea696b228e2e20f93afccd9af4a9cb527bf1fc1
Sergio Patino <sergio@vertigobox.org>
2019-03-04 16:00:54 -0800

    Fixes gopath

--- .config/omf/envvars/go.fish
+++ .config/omf/envvars/go.fish
@@ -1,12 +1,10 @@
 # GOPATH
-[ -f $HOME/Code/go ]; and set -gx GOPATH $HOME/Code/go
-[ -f $HOME/src/go ]; and set -gx GOPATH $HOME/src/go
-# [ -f /usr/local/opt/go/libexec ]; and set -x GOROOT /usr/local/opt/go/libexec
+if test -d $HOME/Code/go
+    set -gx GOPATH $HOME/Code/go
+else if test -d $HOME/src/go
+    set -gx GOPATH $HOME/src/go
+end
 
 if test -n "$GOPATH"
     set PATH $GOPATH/bin $PATH
-end
-
-if test -n "$GOROOT"
-    set PATH $GOROOT/bin $PATH
 end
