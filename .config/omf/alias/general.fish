e436c4ca7773edeed3f8e60aba1e35a7fcaf8084
Sergio Patino <sergio@vertigobox.org>
2020-01-31 15:49:19 -0700

    Update shortcuts

--- .config/omf/alias/general.fish
+++ .config/omf/alias/general.fish
@@ -4,6 +4,23 @@ alias v="nvim"
 if [ $OS = "Darwin" ]
   alias vi="nvim"
 
+  # User GNU date
+  alias date='gdate'
+
+  # Path Shortcuts
+  alias gogithub='cd ~/Code/github'
+  alias gointuit='cd ~/Code/intuit'
+  alias gogo='cd ~/Code/go/src'
+  alias godot='cd ~/Code/github/dotfiles'
+  alias goconf='cd ~/.config'
+  alias goomf='cd ~/.config/omf'
+  alias govim='cd ~/Code/github/vim-config'
+  alias godl='cd ~/Downloads'
+  alias rmt="rmtrash"
+
+  # OMF Reload
+  alias omfr='omf reload'
+
 end
 
 # Determine public IP
@@ -25,7 +42,7 @@ alias tmux="tmux -f $HOME/.config/tmux/tmux.conf"
 [ -f /usr/local/share/nvim/runtime/macros/less.sh ]; and alias le="/usr/local/share/nvim/runtime/macros/less.sh"
 
 # SSH
-alias ss="ssh"
+alias s="ssh"
 
 # Terraform
 alias tf="terraform"
@@ -33,34 +50,28 @@ alias tfp="terraform plan"
 alias tfa="terraform apply"
 
 # History
-alias h="history"
-alias hm="history --merge"
-alias ht="history --show-time"
-alias hs="history search"
+alias h='history'
+alias hm='history --merge'
+alias ht='history --show-time'
+alias hs='history | fzy'
 
-# Clear
-alias c="clear"
+# Cd
+alias c="cd"
 
-# Path Shortcuts
-alias codegit='cd ~/Code/github'
-alias codeintu='cd ~/Code/intuit'
-alias codego='cd ~/Code/go/src'
-alias dot='cd ~/Code/github/dotfiles'
-alias conf='cd ~/conf'
-alias viconf='cd ~/Code/github/vim-config'
 
 # df
-alias dfh='df -HP'
+alias df='df -hi'
 alias tf='tail -1000f'
 
+# Make dir
 alias md='mkdir -p'
 
 # ls
-alias l="ls -AlhF"
+alias l='ls -AlhF'
 alias u='cd ..'
 alias ...='cd ../..'
+alias ....='cd ../../..'
 
-# Use Trash on Darwin
-if [ $OS = "Darwin" ]
-  alias rmt="rmtrash"
-end
+# process lis
+alias ps='ps -Ao user,pid,%cpu,%mem,vsz,rss,tt,stat,start,time,command'
+alias psfind='ps -Ao user,pid,%cpu,%mem,vsz,rss,tt,stat,start,time,command|fzy'
