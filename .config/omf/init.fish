23d2fd5f48ab3a5a4d5f9af6614f1de0d0858ff9
Sergio Patino <sergio@vertigobox.org>
2020-02-10 07:04:19 -0700

    Fix patch

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -13,10 +13,10 @@ if test -n "$GOPATH"
 end
 
 # Add local bin to PATH
-set PATH ~/.local/bin/ $PATH
+set PATH ~/.local/bin $PATH
 
 # Add Homebrew sbin to PATH
-set PATH /usr/local/sbin/ $PATH
+set PATH /usr/local/sbin $PATH
 
 # Source envvars
 for file in $OMF_CONFIG/vars/*.fish
